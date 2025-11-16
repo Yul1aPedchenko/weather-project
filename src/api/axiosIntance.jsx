b51d7e1a32586dcaaa24855192fa212a4ec3ee6b
Yul1aPedchenko <136635600+Yul1aPedchenko@users.noreply.github.com>
2025-11-16 13:53:19 +0200

    Update axiosIntance.jsx

--- src/api/axiosIntance.jsx
+++ src/api/axiosIntance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const weatherApi = axios.create({
-    baseURL: "https://api.openweathermap.org/data/3",
+    baseURL: "https://api.openweathermap.org/data/3.0",
     params: {
         appid: import.meta.env.VITE_OPENWEATHER_KEY,
         units: "metric",
