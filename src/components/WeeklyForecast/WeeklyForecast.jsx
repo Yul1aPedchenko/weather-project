f55970f1af2523d46103bccf43c13d2066705fcc
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2025-11-24 22:04:41 +0200

    .

--- src/components/WeeklyForecast/WeeklyForecast.jsx
+++ src/components/WeeklyForecast/WeeklyForecast.jsx
@@ -1,19 +1,12 @@
-export const WeeklyForecast = ({ city, onClose }) => {
+export const WeeklyForecast = ({ city}) => {
   if (!city) return null;
 
   return (
-    <div className="weekly-modal">
-      <button onClick={onClose} className="close-btn">×</button>
+    <div className="weekly">
       
       <h2>7-Day Forecast — {city.name}, {city.country}</h2>
       
-
-      <p>Тут буде таблиця або картки з тижневим прогнозом</p>
-      
-      <div>
-        <p>Поточна температура: {Math.round(city.temp)}°C</p>
-        <p>Погода зараз: {city.weather}</p>
-      </div>
+  
     </div>
   );
 };
